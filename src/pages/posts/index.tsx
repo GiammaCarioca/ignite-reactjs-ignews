f7233bc84a6399513e14148f2d191e2fc324207e
Thiago Giammattey <giammattey@gmail.com>
2021-03-23 16:21:36 -0300

    style page Posts

---
+++ src/pages/posts/index.tsx
@@ -0,0 +1,37 @@
+import Head from 'next/head'
+import styles from './styles.module.scss'
+
+export default function Posts() {
+  return (
+    <>
+      <Head>
+        <title>Posts | Ignews</title>
+      </Head>
+
+      <main className={styles.container}>
+        <div className={styles.posts}>
+          <a href="">
+            <time>23 de março de 2012</time>
+            <strong>Mapas com React usando Leaflet</strong>
+            <p>Neste post vamos desenvolver uma página web para demonstrar, na prática, a integração de Mapas em uma aplicação com React usando Leaflet.</p>
+          </a>
+          <a href="">
+            <time>23 de março de 2012</time>
+            <strong>Mapas com React usando Leaflet</strong>
+            <p>Neste post vamos desenvolver uma página web para demonstrar, na prática, a integração de Mapas em uma aplicação com React usando Leaflet.</p>
+          </a>
+          <a href="">
+            <time>23 de março de 2012</time>
+            <strong>Mapas com React usando Leaflet</strong>
+            <p>Neste post vamos desenvolver uma página web para demonstrar, na prática, a integração de Mapas em uma aplicação com React usando Leaflet.</p>
+          </a>
+          <a href="">
+            <time>23 de março de 2012</time>
+            <strong>Mapas com React usando Leaflet</strong>
+            <p>Neste post vamos desenvolver uma página web para demonstrar, na prática, a integração de Mapas em uma aplicação com React usando Leaflet.</p>
+          </a>
+        </div>
+      </main>
+    </>
+  )
+}
